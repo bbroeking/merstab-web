7d6ce1e89e7dc43ef66c89ce54f366fb5370eac6
Brian Broeking <bbroeking456@gmail.com>
2022-02-01 17:10:48 -0600

    cleanup

--- components/AppFooter.tsx
+++ components/AppFooter.tsx
@@ -7,9 +7,9 @@ const AppFooter = () => {
         <div className={styles.footerRow}>
             <Image src="/svg/logo.svg" alt='merstab logo' width={60} height={60}></Image>
             <div className={styles.icons}>
-                <a href='https://discord.gg/Nhn8YbsgaE' target="_blank" rel='noopener'><img src="svg/medium.svg" alt="medium icon" height={24} width={24} /></a>
-                <a href='https://twitter.com/merstab' target="_blank" rel='noopener'><img src="svg/twitter.svg" alt="twitter icon" height={24} width={24} /></a>
-                <a href='https://discord.gg/Nhn8YbsgaE' target="_blank" rel='noopener'><img src="svg/discord.svg" alt="discord icon" height={24} width={24} /></a>
+                <a href='https://discord.gg/Nhn8YbsgaE' target="_blank" rel='noreferrer'><img src="svg/medium.svg" alt="medium icon" height={24} width={24} /></a>
+                <a href='https://twitter.com/merstab' target="_blank" rel='noreferrer'><img src="svg/twitter.svg" alt="twitter icon" height={24} width={24} /></a>
+                <a href='https://discord.gg/Nhn8YbsgaE' target="_blank" rel='noreferrer'><img src="svg/discord.svg" alt="discord icon" height={24} width={24} /></a>
                 {/* <a href='https://discord.gg/Nhn8YbsgaE' target="_blank"><img src="svg/gitbook.svg" alt="gitbook icon" height={24} width={24} /></a> */}
             </div>
         </div>
